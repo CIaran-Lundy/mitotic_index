84b2ca57c372c714fcf473703ee846a0ed13ebf2
Ciaran-Lundy <ciaranlundy@gmail.com>
2020-11-25 09:44:16 +0000

    adding future instruction notes

--- read_lmdb.py
+++ read_lmdb.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorpack.dataflow import *
-
+import cv2
 #todo: do something like the following to avoid those weird messages about nvidia:
 #import os
 # set the system environment so that the PCIe GPU ids match the Nvidia ids in nvidia-smi
@@ -11,8 +11,9 @@ lmdb_dir = "/home/ciaran/PycharmProjects/mitotic_index/training_aperio/A03/mitos
 
 output_types= tf.dtypes.int8, tf.dtypes.string
 output_shapes= tf.TensorShape([16, 16]), tf.TensorShape([1, 3])
-#dataset = tf.raw_ops.LMDBDataset(filenames="/home/ciaran/PycharmProjects/mitotic_index/training_aperio/A03/mitosis/lmdb_dir/single_lmdb/data.mdb",output_types=output_types, output_shapes=output_shapes, name=None)
+dataset = tf.raw_ops.LMDBDataset(filenames="/home/ciaran/PycharmProjects/mitotic_index/training_aperio/A03/mitosis/lmdb_dir/single_lmdb/data.mdb",output_types=output_types, output_shapes=output_shapes, name=None)
 
+print(dataset.get_shape())
 
 print("Eager execution: {}".format(tf.executing_eagerly()))
 
@@ -27,4 +28,19 @@ data = tf.compat.v1.LMDBReader(options=lmdb_dir)
 ds = LMDBSerializer.load(lmdb_dir, shuffle=False)
 ds = BatchData(ds, 2, use_list=True)
 TestDataSpeed(ds).start()
-#todo: this should work, now i just need to change the format that the image is saved in
+
+ds = LMDBSerializer.load(lmdb_dir, shuffle=False)
+ds = LocallyShuffleData(ds, 50000)
+ds = MapDataComponent(ds, lambda x: cv2.imdecode(x, cv2.IMREAD_COLOR), 0)
+ds = BatchData(ds, 2)
+
+
+#todo: so then, it seems that ive chosen to make this much harder for myself by trying to implement efficient dataflow.
+# good job idiot. now then, to make this work, im going to need to do the following:
+# make lmdb (done)
+# read some entries from the lmdb into a buffer ( as many as possible for the given amount of ram)
+# shuffle entries in buffer
+# read entries and process them so that they are ready to be presented to the CNN
+# put shuffled entries into a queue
+# make threads that will take entries from this queue and put them into batches
+# make a keras model that will ask for a batche, wait until it runs through the model, and then ask for another batch - keras fit_generator might be a shout for this
